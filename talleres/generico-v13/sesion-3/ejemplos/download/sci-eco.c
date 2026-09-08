6d7b4e870774478a8aabb04c0f237c35b18420b2
obijuan <obijuan.cube@gmail.com>
2026-09-08 07:15:42 +0200

    add talleres/generico-v13

---
+++ talleres/generico-v13/sesion-3/ejemplos/download/sci-eco.c
@@ -0,0 +1,48 @@
+/*************************************************************************** */
+/* sci-eco.c      Julio-2005                                                 */
+/*---------------------------------------------------------------------------*/
+/* Ejemplo para el skybot                                                    */
+/*---------------------------------------------------------------------------*/
+/* Se hace eco de todo lo recibido por el puerto serie                       */
+/*---------------------------------------------------------------------------*/
+/*  Juan Gonzalez <juan@iearobotics.com>                                     */
+/*---------------------------------------------------------------------------*/
+/*  LICENCIA GPL                                                             */
+/*****************************************************************************/
+//-- Especificar el pic a emplear
+#define __16f877
+#include "pic16f877.h"
+
+//-- Se usa la libreria de comunicaciones serie
+#include "sci.h"
+
+
+/*********************************/
+/* Programa principal            */
+/*********************************/
+void main(void)
+{
+  unsigned char c;
+
+  //-- Configurar el puerto B 
+  //-- Todos los pines de entrada, salvo RB1 de salida, que es
+  //-- donde esta el led
+  TRISB=0xFD;
+  
+  //-- Configurar las comunicaciones serie
+  sci_conf();
+  
+  //-- Bucle infinito
+  for (;;) {
+    
+    //-- Esperar a que llegue dato del PC
+    c=sci_read();
+    
+    //-- Cambiar el led de estado
+    PORTB^=0x02; 
+    
+    //-- ...y enviar de vuelta el caracter por el puerto serie
+    sci_write(c);
+  }
+
+}
---
+++ talleres/generico-v13/sesion-4/download/bumper.c
@@ -0,0 +1,47 @@
+/*************************************************************************** */
+/* bumpers.c      Febrero-2006                                               */
+/*---------------------------------------------------------------------------*/
+/* Ejemplo para el skybot                                                    */
+/*---------------------------------------------------------------------------*/
+/*  Ejemplo de prueba para los bumpers.                                      */
+/*  Al apretar el Bumper1 (Derecho) se apaga el LED                          */
+/*---------------------------------------------------------------------------*/
+/*  Andres Prieto-Moreno <andres@ifara.com>                                  */
+/*  Juan Gonzalez <juan@iearobotics.com>                                     */
+/*---------------------------------------------------------------------------*/
+/*  LICENCIA GPL                                                             */
+/*****************************************************************************/
+
+//-- Especificar el pic a emplear
+#define __16f877
+#include "pic16f877.h"
+
+#define DERECHO   0x02 //Bumper 1  Derecho desde arriba
+#define IZQUIERDO 0x04 //Bumper 2  Iquierdo desde arriba
+
+//----------------------------
+//- Comienzo del programa  
+//----------------------------
+
+unsigned char bumper;
+void main(void)
+{
+  //-- Configurar el puerto B para trabajar con el Skybot
+  //-- RB0, RB5, RB6 y RB7 como entradas
+  //-- RB1, RB2, RB3 y RB4 como salidas
+  TRISB=0xE1;
+  
+  //-- Configurar el puerto A para trabajar con los bumpers
+  ADCON1=0x0E;
+  
+  for(;;) {
+    bumper=PORTA & DERECHO; // Leer bumper 1
+	if ( bumper==0) { 
+		PORTB=0x02;
+	} else {    // al apretar el bumper leemos 1
+		PORTB=0;
+	}
+	
+  }
+
+}
